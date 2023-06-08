8197196252c7e3c56555df9a16ad1f7c5ddfa139
Zhang <panpan.zhang@vumc.org>
2023-06-08 15:05:37 -0500

    try to add a map

---
+++ smimap/org-locations.js
@@ -0,0 +1,46 @@
+var addressPoints = [
+  [
+    "Philadelphia, PA, USA.",
+    39.9526,
+    -75.1652
+  ],
+  [
+    "Storrs, CT, USA",
+    41.8084,
+    -72.2495
+  ],
+  [
+    "Washington DC, USA",
+    38.9072,
+    -77.0369
+  ],
+  [
+    "Winston-Salme, NC, USA",
+    36.0999,
+    -80.2442
+  ],
+  
+  [
+    "Shanghai, PRC",
+    31.2304,
+    121.4737
+  ],
+  
+  [
+    "Sydney, NSW, AU",
+    -33.8688,
+    151.2093
+  ],
+  
+  [
+    "Nashville, TN, USA",
+    36.1627,
+    -86.7816
+  ],
+  
+   [
+    "Budapest, HU",
+    47.4979,
+    19.0402
+  ],
+];
