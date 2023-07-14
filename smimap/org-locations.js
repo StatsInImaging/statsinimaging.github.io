9682908c52d905b39c9a344dd8da7b6ceacbfe31
Zhang <panpan.zhang@vumc.org>
2023-07-13 19:00:31 -0500

    update SMI map

--- smimap/org-locations.js
+++ smimap/org-locations.js
@@ -1,42 +1,42 @@
 var addressPoints = [
   [
-    "Ann Arbor, MI, USA",
+    "SMI 2015, University of Michigan",
     42.2808,
     -83.7430
   ],
   [
-    "Boulder, CO, USA",
+    "SMI 2016, University of Corolado",
     40.0150,
     -105.2705
   ],
   [
-    "Pittsburgh, PA, USA",
+    "SMI 2017, University of Pittsburgh",
     40.4406,
     -79.9959
   ],
   [
-    "Philadelphia, PA, USA.",
+    "SMI 2018, University of Pennsylvania",
     39.9526,
     -75.1652
   ],
   [
-    "Irvine, CA, USA",
+    "SMI 2019, University of California Irvine",
     33.6846,
     -117.8265
   ],
   
   [
-    "Atlanta, GA, USA",
+    "SMI 2021, Emory University",
     33.7488, 
     -84.3877
   ],
   [
-    "Nashville, TN, USA",
+    "SMI 2022, Vanderbilt University",
     36.1627,
     -86.7816
   ],
    [
-    "Minneapolis, MN, USA",
+    "SMI 2023, University of Minnesota",
     44.9778, 
     -93.2650
   ],
